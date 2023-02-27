3cc011089502c3f82f8070acca282cea29fb3f33
bcl <barrylui88@googlemail.com>
2023-02-27 16:37:17 +0000

    Added DS Store to gitignore and written out all questions for Inquirer

--- index.js
+++ index.js
@@ -5,7 +5,51 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "projectName"
+    },
+    {
+        type: "input",
+        message: "Please enter a description of your project.",
+        name: "projectDescription"
+    },
+    {
+        type: "input",
+        message: "Please enter the Installation Instructions for your project.",
+        name: "projectInstallationInstructions"    
+    },
+    {
+        type:"input",
+        message: "Please enter any Usage Information for your project.",
+        name: "projectUsageInformation"
+    },
+    {
+        type:"input",
+        message: "Please enter any Contribution Guidelines for your project.",
+        name: "projectContributionGuidelines"
+    },
+    {
+        type:"input",
+        message: "Please enter any Test Instructions for your project.",
+        name: "projectTestInstructions"
+    },
+    {
+        type:"checkbox",
+        message: "Please choose a license for your application from the list of options below.",
+        choices: ["MIT","General Public License","Apache"]
+    },
+    {
+        type:"input",
+        message: "What is your GitHub Username?",
+        name: "githubUsername"
+    },
+    {
+        type:"input",
+        message: "What is your email address?",
+        name: "emailAddress"
+    }
 ];
 
 // function to write README file
